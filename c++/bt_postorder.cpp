ddc60b5f4c3424dbf36fdcea03fad3d7302d54f5
nimishjs17 <47712914+nimishjs17@users.noreply.github.com>
2023-03-21 03:14:22 +0530

    sync

--- c++/bt_postorder.cpp
+++ c++/bt_postorder.cpp
@@ -1,28 +1,45 @@
 #include<stdio.h>
-#include<iostreamm.h>
+#include<iostream>
 
 using namespace std;
 
-struct *Node
+struct Node
 {
     int data;
-    Node *left;
-    Node *right;
+    Node* left;
+    Node* right;
+};
+
+Node* setNode(int data)
+{
+    Node* tempNode = new Node;
+    tempNode->data = data;
+    tempNode->left = NULL;
+    tempNode->right = NULL;
+
+    return tempNode;
 }
 
-void postorder(Node& node);
-
-void main()
+void InOrder(Node* root)
 {
-
-}
-
-void postorder(Node& node)
-{
-    if(node != null)
+    if(root == NULL)
     {
-        postorder(node->node.left);
-        cout<<node->data;
-        postorder(node->node.right);
+        return;
     }
+    InOrder(root->left);
+    cout<<root->data<<", ";
+    InOrder(root->right);
+}
+
+int main()
+{
+    Node* root = setNode(1);
+    root->left = setNode(2);
+    root->right = setNode(3);
+    root->left->left = setNode(4);
+    root->left->right = setNode(5);
+
+    InOrder(root);
+
+    return 0;
 }
